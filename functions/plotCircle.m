a72e82ab19bd38d0c3870c5629ecad5e0b690cf1
kevinsan1 <kevin.mead1@hotmail.com>
2014-05-16 17:38:03 +0200

    Plots minDist with circle function on mesh

---
+++ functions/plotCircle.m
@@ -0,0 +1,20 @@
+function [ output_args ] = plotCircle(xc,yc,r,size)
+% plotCircle = (x,y,r) 
+% (x,y) = Center Point
+% r = radius
+n = size;
+theta = 0 : 2*pi/n : 2*pi;
+x = r.*cos(theta) + xc;
+y = r.*sin(theta) + yc;
+figure('Units', 'pixels', ...
+    'Position', [100 100 500 375]);
+hold on;
+plot(x,y)
+hold on;
+plot(xc,yc,'.')
+axis([-1 1 -1 1])
+% axis('square');
+
+
+end
+
--- signedDistance.m
+++ signedDistance.m
@@ -1,17 +1,17 @@
-clear all;clc;myPath = ['/Users/kevin/SkyDrive/KTH Work/',...
+clear all;clc;close all;
+myPath = ['/Users/kevin/SkyDrive/KTH Work/',...
     'Period 3 2014/DN2255/Homework/5/'];
 addpath(genpath(myPath));
+%% Define constants
+n = 200;
 %% Define x and y on T
-thetaMax = 2*pi;
-rMax = 1;
-n = 100;
-theta = 0 : thetaMax/n : thetaMax;
+rMax = 0.3;
+theta = 0 : 2*pi/n : 2*pi;
 r = rMax;
-x = r.*cos(theta);
-y = r.*sin(theta);
-%% Plot figure
-% figure(1);clf
-% plot(x,y)
+xc = 0;
+yc = -0.6;
+x = r.*cos(theta) + xc;
+y = r.*sin(theta) + yc;
 %% Make grid of x and y values
 [boundaryX, boundaryY] = meshgrid(-1:2/n:1,-1:2/n:1);
 %% Preallocation
@@ -27,6 +27,11 @@ for i = 1:(n+1)
         [minDist(i,k),index] = min(d);
     end
 end
+clear i j k;
 
-
-
+%% Plot minDist
+% mesh(boundaryX, boundaryY,minDist)
+% hold on;
+% mesh(x,y,zeros(n+1,n+1))
+% %% Plot initial circle
+% plotCircle(xc,yc,r,n);
