eba9ae8101b796552a5906cf2651c51888254bc2
kevinsan1 <kevin.mead1@hotmail.com>
2014-05-17 18:25:46 +0200

    Revert e437317..c930f60
    This rolls back to commit e4373171141131208baf49a9a3a86e5e1b9274df.

--- levelSetMethod.m
+++ levelSetMethod.m
@@ -16,26 +16,35 @@ dy = dx;
 % uext = (u,v)
 % u = -cos(pi*(boundaryX+1/2)) .* sin(3*pi/8*boundaryY);
 % v =  sin(pi*(boundaryX+1/2)) .* cos(3*pi/8*boundaryY);
-
 g = 2:n+1;
-u=2+cos(2*pi*Y);
-v=2+sin(2*pi*X);
-u = setBoundariesOfNxN(u,1);
-v = setBoundariesOfNxN(v,1);
+u(g,g)=2+cos(2*pi*Y);
+v(g,g)=2+sin(2*pi*X);
+u(1,:)      = u(2,:);
+u(end,:)    = u(end-1,:);
+v(1,:)      = v(2,:);
+v(end,:)    = v(end-1,:);
 tFinal = 1;
 dt = 0.1*dx;
-tSteps = 10;%ceil(tFinal/dt);
+tSteps = ceil(tFinal/dt);
 %% Signed Distance Function
-phi = signedDistance;
-phi = setBoundariesOfNxN(phi,1);
-phin = zeros(n,n);
-
+phi = zeros(n+2,n+2);
+phi(g,g,1) = signedDistance;
+phin = zeros(n+2,n+2);
+%% Define x and y on T
+theta = 0 : 2*pi/n : 2*pi;
+xc = 0;
+yc = -0.6;
+x = r.*cos(theta) + xc;
+y = r.*sin(theta) + yc;
+%% Plot initial phi
+% figure(1)
+% mesh(boundaryX,boundaryY, phi(:,:,1))
+% xlabel('x')
+% ylabel('y')
+% i = 1;
 %% Set u and v
-im = [1,[1:n]];
-im = setBoundariesOfNxN(im,0);
-ip = [[2:n],n];
-ip = setBoundariesOfNxN(ip,0);
-%%
+im = [1,[1:n],n];
+ip = [2,[2:n],n];
 up = u(im,:);
 um = u(ip,:);
 up(up<=0) = 0;
@@ -43,7 +52,6 @@ um(um>=0) = 0;
 vp = v(:,im);vm = v(:,ip);
 vp(vp<=0) = 0;
 vm(vm>=0) = 0;
-phiPlot(:,:,1) = phi;
 %% Main loop
 for tn = 1:tSteps
     for i = g
@@ -52,20 +60,22 @@ for tn = 1:tSteps
             wxp = phi(ip(i),j,tn) - phi(i,j,tn); 	% x forward difference
             wym = phi(i,j,tn) - 	phi(i,im(j),tn); 	% y backward difference
             wyp = phi(i,ip(j),tn) - phi(i,j,tn); 	% y forward difference
-            phin(1:n,1:n) = phi(i,j,tn)...
+            phin(i,j) = phi(i,j,tn)...
                 -dt/dx * ( max(u(im(i),j),0)*wxm + min(u(ip(i),j),0)*wxp )...
                 -dt/dy * ( max(v(i,ip(j)),0)*wym + min(v(i,ip(j)),0)*wyp );
         end
     end
-    phin = setBoundariesOfNxN(phin,1);
-    phiPlot(:,:,tn+1) = phin;
-    
+    phin(1,:) 		= phin(2,:);
+    phin(:,1) 		= phin(:,2);
+    phin(end,:) 	= phin(end-1,:);
+    phin(:,end)     = phin(:,end-1);
+    phi(:,:,tn+1) = phin;
 end
 toc
 %% Plotting
 figure(2)
 for iPlot = 1:tSteps
-    contour(X,Y,phiPlot(g,g,1),[0,0],'r');
+    contour(X,Y,phi(g,g,iPlot),[0,0],'r');
     axis([-1 1 -1 1])
     axis('square')
     title(sprintf('t = %0.3g',iPlot*dt));
--- test.m
+++ test.m
@@ -1,5 +1,9 @@
-clearvars
-n=10;
-im = (1:n);
-setBoundariesOfNxN(im)
-% [sx,sy] = size(im);
+% clear all;clc;close all;
+clearvars -except phi
+a = magic(5)
+b = zeros(7)
+b(2:6,2:6) = a;
+b(1,:) = b(2,:)
+b(:,1) = b(:,2)
+b(end,:) = b(end-1,:)
+b(:,end)=b(:,end-1)
