7559e4747124e317f5fabf96b15325fd391e057d
kevinsan1 <kevin.mead1@hotmail.com>
2014-05-20 17:02:26 +0200

    Revert 46ff245..c2e7f0d
    This rolls back to commit 46ff245aa9dad08e5443589d4df64863a1dfa469.

--- levelSetMethod.m
+++ levelSetMethod.m
@@ -13,31 +13,24 @@ L = 2;
 dx = L/(n-1);
 dy = dx;
 % Make grid of x and y values
-<<<<<<< HEAD
-[X, Y] = meshgrid(-L/2:dx:L/2);
+[X, Y] = meshgrid(-L/2:dx:L/2,-L/2:dy:L/2);
 tFinal = 1.6;
 dt = .1*dx;
 tSteps = ceil(tFinal/dt);
-=======
-[X, Y] = meshgrid(-L/2:dx:L/2,-L/2:dy:L/2);
-tFinal = .1;
-dt = .01*dx;
-tSteps = 16*10 %ceil(tFinal/dt);
->>>>>>> parent of 46ff245... good, making changes I'm ip
 ia = 1:n;
 im = [n,1:n-1];
 ip = [2:n,1];
 %% Signed Distance Function
 phi = signedDistance;
 %% Define velocity field
-%     u=2+cos(2*pi*Y);
-%     v=2+sin(2*pi*X);
+%     u=.8+cos(2*pi*Y);
+%     v=.8+sin(2*pi*X);
 u=-cos(pi*(X+0.5)).*sin(3*pi/8*Y);
 v=sin(pi*(X+0.5)).*cos(3*pi/8*Y);
-up = u(:,:);
-um = u(:,:);
-vp = v(:,:);
-vm = v(:,:);
+up = u;
+um = u;
+vp = v;
+vm = v;
 up(up<=0) = 0;
 um(um>=0) = 0;
 vp(vp<=0) = 0;
@@ -52,33 +45,24 @@ phiplot(:,:,1) = phi;
 %% Main loop
 myHandle = waitbar(0,'Initializing waitbar...');
 tic;
+
 for tn = 1:tSteps
-    for i = n:-1:1
+    for i = 1:n
         for j = 1:n
-<<<<<<< HEAD
             wxm = phi(i,j) - phi(im(i),j);	% x backward difference
             wxp = phi(ip(i),j) - phi(i,j); 	% x forward difference
             wym = phi(i,j) - phi(i,im(j)); 	% y backward difference
             wyp = phi(i,ip(j)) - phi(i,j); 	% y forward difference
-            fx(i,j) = max(u(im(i),j),0)*wxm + min(u(ip(i),j),0)*wxp;
-            fy(i,j) = max(v(i,im(j)),0)*wym + min(v(i,ip(j)),0)*wyp;
+            fx(i,j) = up(im(i),j)*wxm + um(ip(i),j)*wxp;
+            fy(i,j) = vp(i,im(j))*wym + vm(i,ip(j))*wyp;
             phinew(i,j) = phi(i,j)-dt*( fx(i,j)/dx + fy(i,j)/dy );
-=======
-            wxm = phi(i,j,tn) - phi(im(i),j,tn);	% x backward difference
-            wxp = phi(ip(i),j,tn) - phi(i,j,tn); 	% x forward difference
-            wym = phi(i,j,tn) - phi(i,im(j),tn); 	% y backward difference
-            wyp = phi(i,ip(j),tn) - phi(i,j,tn); 	% y forward difference
-            phin(i,j) = phi(i,j,tn)...
-                -dt/dx * ( up(i,j)*wxm - um(i,j)*wxp )...
-                -dt/dy * ( vp(i,j)*wym - vm(i,j)*wyp );
->>>>>>> parent of 46ff245... good, making changes I'm ip
         end
     end
     % phin = setBoundariesOfNxN(phin,phiBC);
-    phi(:,:,tn+1) = phin(:,:);
+    phi = phinew;
     if( rem(tSteps,plotStep) < 1 )  % Every plot_iter steps record
         iplot = iplot+1;
-        phiplot(:,:,iplot) = phi(:,:,tn+1);       % Record a(i) for ploting
+        phiplot(:,:,iplot) = phi;       % Record a(i) for ploting
         tplot(iplot) = dt*tn;
     end
     %% Here's the progress bar code
@@ -101,13 +85,14 @@ end
 qp = 1:round(n/25):n;
 % u=2-cos(pi*(X+0.5)).*sin(3*pi/8*Y);
 % v=2+sin(pi*(X+0.5)).*cos(3*pi/8*Y);
-
+quivU = up(im(:),:) + um(ip(:),:);
+quivV = vp(:,im(:)) + vm(:,ip(:));
 for iPlot = 1:iplot
     figure(2)
     contour(X,Y,phiplot(:,:,iPlot),[0,0],'r');
     hold on;
     contour(X,Y,phiplot(:,:,1),[0,0],'b');
-    quiver(X(qp,qp),Y(qp,qp),u(qp,qp),v(qp,qp))
+    quiver(X(qp,qp),Y(qp,qp),quivU(qp,qp),quivV(qp,qp))
     hold off
     axis([-L/2 L/2 -L/2 L/2])
     axis('square')
